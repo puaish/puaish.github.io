0cb220c26949e35f104b10dc62ae8b0ae9f726b9
qiaoronggui <qiaoronggui@qiyu3.onaliyun.com>
2023-08-31 14:00:34 +0800

    8/31/2023

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2023/08/31/hello-world/","2023/08/30/My-New-Post/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[];
+var posts=["2023/08/30/My-New-Post/","2023/08/31/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[];
     var refreshNum = 1;
     function friendChainRandomTransmission() {
       const randomIndex = Math.floor(Math.random() * friend_link_list.length);
